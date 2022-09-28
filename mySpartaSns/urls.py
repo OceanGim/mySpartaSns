070af4206357662432b3b082c3860d628361d81c
OceanGim <rladlsghrk@gmail.com>
2022-09-28 15:00:20 +0900

    modified mySpartaSns/urls.py add include user.urls

--- mySpartaSns/urls.py
+++ mySpartaSns/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.base_response,name='first_test'),   
     path('first/', views.first_view,name='first_view'),
+    path('', include('user.urls')),
 ]
